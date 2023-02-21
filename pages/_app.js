6c7bb2b5faa23ae5a87cae5cf96b0259fa0e02d0
Olanrewaju Bayode <lanrebayode77@gmail.com>
2023-02-21 08:50:16 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
 import Style from "@/styles/globals.css";
-import { Roboto } from "@next/font/google";
+//import { Roboto } from "@next/font/google";
 
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
+// const roboto = Roboto({
+//   weight: "400",
+//   subsets: ["latin"],
+// });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={roboto.className}>
-      <div className={Style.main}>
-        <Component {...pageProps} />
-      </div>
-    </main>
+    <div className={Style.main}>
+      <Component {...pageProps} />
+    </div>
   );
 }
